45404c367c928b650c624c3060853bfefa7fe340
NghiaInter0403 <phanhieunghia13052019@gmail.com>
2025-11-16 14:33:41 +0700

    sua lai drop down menu

--- include_header.php
+++ include_header.php
@@ -37,6 +37,7 @@
 
                             <?php if ($_SESSION['role'] == 'sinhvien'): ?>
                                 <li><a class="dropdown-item" href="sv_trangchu.php">Trang cá nhân</a></li>
+                                <li><a class="dropdown-item" href="dangxuat.php">Đăng xuất</a></li>
                             <?php elseif ($_SESSION['role'] == 'nhatuyendung'): ?>
                                 <li><a class="dropdown-item" href="cty_trangchu.php">Trang chủ</a></li>
                                 <li><a class="dropdown-item" href="cty_dangtuyen.php">Đăng tin</a></li>
@@ -45,9 +46,8 @@
                                 <li><a class="dropdown-item" href="dangxuat.php">Đăng xuất</a></li>
                             <?php elseif ($_SESSION['role'] == 'quatrivien'): ?>
                                 <li><a class="dropdown-item" href="ad_trangchu.php">Quản trị</a></li>
+                                <li><a class="dropdown-item" href="dangxuat.php">Đăng xuất</a></li>
                             <?php endif; ?>
-                            <li><hr class="dropdown-divider"></li>
-                            <li><a class="dropdown-item" href="dangxuat.php">Đăng xuất</a></li>
                            </ul>
                     </li>
                 <?php else: ?>
