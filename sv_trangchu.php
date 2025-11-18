cef4ed4b87052e5bcd7e932212dbd44bc137f992
NghiaInter0403 <phanhieunghia13052019@gmail.com>
2025-11-18 15:50:56 +0700

    sua phan footer cho xuong duosi

--- sv_trangchu.php
+++ sv_trangchu.php
@@ -9,7 +9,7 @@ $user_id = $_SESSION['user_id'];
 ?>
 
 <?php include 'include_header.php'; ?>
-<div class="container mt-4">
+<div class="container mt-4" style="height: 500px;">
     <h2>Xin chào, <?php echo $_SESSION['name']; ?>!</h2>
     <div class="row mt-4">
         <div class="col-md-4">
