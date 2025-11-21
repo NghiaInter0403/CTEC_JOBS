b1ccd1c1d1e3baa52d6b61a89631d4691a7b5d53
NghiaInter0403 <phanhieunghia13052019@gmail.com>
2025-11-21 21:57:46 +0700

    chỉnh giao diện

--- include_header.php
+++ include_header.php
@@ -7,10 +7,9 @@
     <title>Việc Làm Sinh Viên</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
-    <link rel="stylesheet" href="<?= $root ?>assets/css/style.css">
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    <link rel="stylesheet" href="style.css">
 </head>
 <body>
 <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
