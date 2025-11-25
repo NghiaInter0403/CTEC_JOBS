7d9d09e4914a9f0127dda7ddd4b06c4a87f9bace
Huynh Tan Phat <phtproqua@gmail.com>
2025-11-25 21:05:11 +0700

    cap nhat ten bien

--- ad_qlyviec.php
+++ ad_qlyviec.php
@@ -48,7 +48,7 @@ $result = mysqli_query($conn, $sql);
                 <td>
                     <?php if ($vieclam['trangthai'] == 'choxuly'): ?>
                         <!-- <a href="?choxuly=<?php echo $vieclam ['id']; ?>" class="btn btn-sm btn-success">Chờ xử lý</a> -->
-                        <a href="?daduyet=<?php echo $job['id']; ?>" class="btn btn-sm btn-success">Duyệt</a>
+                        <a href="?daduyet=<?php echo $vieclam ['id']; ?>" class="btn btn-sm btn-success">Duyệt</a>
                         <a href="?tuchoi=<?php echo $job['id']; ?>" class="btn btn-sm btn-danger">Từ chối</a>
                     <?php endif; ?>
                 </td>
