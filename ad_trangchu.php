5fc760107a31f6ca0ba0fecb556baa98b9802bdf
Huynh Tan Phat <phtproqua@gmail.com>
2025-11-27 18:26:15 +0700

    cap nhat ten bien

--- ad_trangchu.php
+++ ad_trangchu.php
@@ -126,7 +126,7 @@ $ungvienmoi = mysqli_query($conn, "SELECT a.*, j.tieude, u.hoten as sinhvien FRO
                     </h5>
                 </div>
                 <div class="card-body p-0">
-                    <?php if (mysqli_num_rows($recent_apps) > 0): ?>
+                    <?php if (mysqli_num_rows($ungvienmoi) > 0): ?>
                     <div class="list-group list-group-flush">
                         <?php while ($app = mysqli_fetch_assoc($recent_apps)): ?>
                         <div class="list-group-item d-flex justify-content-between align-items-center">
