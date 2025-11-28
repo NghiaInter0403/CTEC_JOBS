aacd4cc13d1f357ae845bbbcc9073519989e1b4f
NghiaInter0403 <phanhieunghia13052019@gmail.com>
2025-11-28 10:01:05 +0700

    chinh sua file thongke.phop

--- thongke.php
+++ thongke.php
@@ -5,10 +5,10 @@ if (!isset($_SESSION['user_id']) || $_SESSION['role'] != 'quantrivien') {
     exit;
 }
 include 'ketnoi.php';
-
-$total_jobs = mysqli_fetch_assoc(mysqli_query($conn, "SELECT COUNT(*) as total FROM vieclam WHERE trangthai='daduyet'"))['total'];
-$total_applications = mysqli_fetch_assoc(mysqli_query($conn, "SELECT COUNT(*) as total FROM donungvien"))['total'];
-$total_students = mysqli_fetch_assoc(mysqli_query($conn, "SELECT COUNT(*) as total FROM nguoidung WHERE vaitro='sinhvien'"))['total'];
+// $total_jobs = $tong_vieclam $total_applications= $tong_ungtuyen $total_students = $tong_sinhvien
+$tong_vieclam = mysqli_fetch_assoc(mysqli_query($conn, "SELECT COUNT(*) as total FROM vieclam WHERE trangthai='daduyet'"))['total'];
+$tong_ungtuyen = mysqli_fetch_assoc(mysqli_query($conn, "SELECT COUNT(*) as total FROM donungvien"))['total'];
+$tong_sinhvien = mysqli_fetch_assoc(mysqli_query($conn, "SELECT COUNT(*) as total FROM nguoidung WHERE vaitro='sinhvien'"))['total'];
 ?>
 
 <?php include 'include_header.php'; ?>
@@ -18,7 +18,7 @@ $total_students = mysqli_fetch_assoc(mysqli_query($conn, "SELECT COUNT(*) as tot
         <div class="col-md-3">
             <div class="card text-center bg-info text-white">
                 <div class="card-body">
-                    <h4><?php echo $total_jobs; ?></h4>
+                    <h4><?php echo $tong_vieclam; ?></h4>
                     <p>Tin tuyển dụng</p>
                 </div>
             </div>
@@ -26,7 +26,7 @@ $total_students = mysqli_fetch_assoc(mysqli_query($conn, "SELECT COUNT(*) as tot
         <div class="col-md-3">
             <div class="card text-center bg-success text-white">
                 <div class="card-body">
-                    <h4><?php echo $total_applications; ?></h4>
+                    <h4><?php echo $tong_ungtuyen; ?></h4>
                     <p>Ứng tuyển</p>
                 </div>
             </div>
@@ -34,7 +34,7 @@ $total_students = mysqli_fetch_assoc(mysqli_query($conn, "SELECT COUNT(*) as tot
         <div class="col-md-3">
             <div class="card text-center bg-warning text-white">
                 <div class="card-body">
-                    <h4><?php echo $total_students; ?></h4>
+                    <h4><?php echo $tong_sinhvien; ?></h4>
                     <p>Sinh viên</p>
                 </div>
             </div>
