238a91fd0229956581e9ba8659bed983352a5a46
Huynh Tan Phat <phtproqua@gmail.com>
2025-11-29 10:08:36 +0700

    cap nhat hien thi trang thai cho quan ly user

--- ad_qlyviec.php
+++ ad_qlyviec.php
@@ -42,7 +42,7 @@ $result = mysqli_query($conn, $sql);
                 <td><?php echo $vieclam ['mota']; ?></td>
                 <td>
                     <span class="badge bg-<?php echo $vieclam ['trangthai'] == 'daduyet' ? 'success' : ($vieclam ['trangthai'] == 'tuchoi' ? 'danger' : 'warning'); ?>">
-                        <?php echo ucfirst($vieclam ['trangthai']); ?>
+                        <?php echo ucfirst($vieclam ['trangthai'])=='daduyet'?'Đã duyệt':($vieclam['trangthai']=='tuchoi'?'Từ chối':'Chờ xử lý'); ?>
                     </span>
                 </td>
                 <td>
