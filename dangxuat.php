e48723118a5d934d88d4ea3478aebfd8a8440a84
NghiaInter0403 <phanhieunghia13052019@gmail.com>
2025-11-29 17:50:41 +0700

    chinh sua file dangxuat

--- dangxuat.php
+++ dangxuat.php
@@ -1,14 +1,8 @@
 <?php
 session_start();
-
-// Xóa toàn bộ session
 session_unset();
 session_destroy();
-
-// Xóa cookie nếu có
 setcookie('user_id', '', time() - 3600, "/");
-
-// Chuyển về trang chủ
 header("Location: index.php");
 exit;
 ?>
