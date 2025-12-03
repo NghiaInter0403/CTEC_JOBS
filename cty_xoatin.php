09ab4a16f5efccf4bd4b155ab857e550bd031c1b
Huynh Tan Phat <phtproqua@gmail.com>
2025-12-03 22:00:54 +0700

    sua ten bien

--- cty_xoatin.php
+++ cty_xoatin.php
@@ -1,11 +1,11 @@
 <?php
 session_start();
-if (!isset($_SESSION['user_id']) || $_SESSION['role'] != 'nhatuyendung') {
+if (!isset($_SESSION['id_nguoidung']) || $_SESSION['vaitro'] != 'nhatuyendung') {
     exit;
 }
 include 'ketnoi.php';
 $id_vieclam = $_GET['id'];
-$id_nhatuyendung = $_SESSION['user_id'];
+$id_nhatuyendung = $_SESSION['id_nguoidung'];
 
 mysqli_query($conn, "DELETE FROM vieclam WHERE id = '$id_vieclam' AND idnhatuyendung = '$id_nhatuyendung'");
 header("Location: cty_trangchu.php");
