95f165451a0413a2fa88b6e7ea1edcdc603a13a0
Huynh Tan Phat <phtproqua@gmail.com>
2025-12-03 21:58:30 +0700

    sua ten bien

--- cty_dangtuyen.php
+++ cty_dangtuyen.php
@@ -1,11 +1,11 @@
 <?php
 session_start();
-if (!isset($_SESSION['user_id']) || $_SESSION['role'] != 'nhatuyendung') {
+if (!isset($_SESSION['id_nguoidung']) || $_SESSION['vaitro'] != 'nhatuyendung') {
     header("Location: login.php");
     exit;
 }
 include 'ketnoi.php';
-$id_nhatuyendung = $_SESSION['user_id'];
+$id_nhatuyendung = $_SESSION['id_nguoidung'];
 
 if ($_POST) {
     $tieude = $_POST['tieude'];
