4e1e3823bb71c69b7b30ab861650ac32edeffc85
Huynh Tan Phat <phtproqua@gmail.com>
2025-12-03 21:59:36 +0700

    sua ten bien

--- cty_trangchu.php
+++ cty_trangchu.php
@@ -1,11 +1,11 @@
 <?php
 session_start();
-if (!isset($_SESSION['user_id']) || $_SESSION['role'] != 'nhatuyendung') {
+if (!isset($_SESSION['id_nguoidung']) || $_SESSION['vaitro'] != 'nhatuyendung') {
     header("Location: login.php");
     exit;
 }
 include 'ketnoi.php';
-$id_nhatuyendung = $_SESSION['user_id'];
+$id_nhatuyendung = $_SESSION['id_nguoidung'];
 
 // Thống kê
 $tongtin = mysqli_fetch_assoc(mysqli_query($conn, "SELECT COUNT(*) as total FROM vieclam WHERE idnhatuyendung = '$id_nhatuyendung'"))['total'];
