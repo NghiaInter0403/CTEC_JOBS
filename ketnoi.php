11a5021e90f7f3619bc3368c3ca6be9dd1dfe703
Huynh Tan Phat <phtproqua@gmail.com>
2025-12-03 20:32:35 +0700

    cap nhat bien

--- ketnoi.php
+++ ketnoi.php
@@ -3,7 +3,7 @@ $host = "localhost";
 $tendata = "ctecjobs";
 $user = "root";
 $matkhau = "";
-    $conn = mysqli_connect($host, $user, $pass, $tendata);
+    $conn = mysqli_connect($host, $user, $matkhau, $tendata);
     if (!$conn) {
     die("Kết nối thất bại: " . mysqli_connect_error());
 }
